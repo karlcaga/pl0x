4a42220067f659a68e43f0034eab930d5ba77522
Karl Cagalawan <karl.cagalawan@gmail.com>
2024-08-25 23:05:30 -0400

    Add --parse option to CLI

--- pplox/cli.py
+++ pplox/cli.py
@@ -2,6 +2,8 @@
 import sys
 import argparse
 from .scanner import Scanner
+from .parser import Parser
+from .ast_printer import AstPrinter
 
 def main():
     parser = argparse.ArgumentParser(
@@ -10,16 +12,20 @@ def main():
     )
     parser.add_argument('filename')           
     parser.add_argument('-t', '--tokenize', action='store_true')
+    parser.add_argument('-p', '--parse', action='store_true')
     args = parser.parse_args()
 
     with open(args.filename) as file:
         file_contents = file.read()
     
-    if args.tokenize:
+    if args.tokenize or args.parse:
         scanner = Scanner(file_contents)
         tokens = scanner.scan_tokens()
         for token in tokens:
             print(token.to_string())
+        if args.parse:
+            parser = Parser(tokens)
+            AstPrinter().print(parser.parse())
     else:
         print("Interpreter not yet implemented", file=sys.stderr)
         sys.exit(1)
